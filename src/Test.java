bb1fdc5c27abf07148cb1abd6a1703df5c68e004
filpenkosan9 <filipenkosan9@mail.ru>
2017-06-25 20:17:40 +0300

    ИЗМ2

--- src/Test.java
+++ src/Test.java
@@ -4,5 +4,9 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("hello world");
+
+        System.out.println("все получилось");
+
+        System.out.println("WATAFUCK??");
     }
 }
