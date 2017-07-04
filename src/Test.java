10821b08f4900d3e616fb48284f4dc20abc48eb8
filpenkosan9 <filipenkosan9@mail.ru>
2017-07-04 22:10:48 +0300

    mod2

--- src/Test.java
+++ src/Test.java
@@ -1,12 +1,29 @@
+import java.util.Scanner;
+
 /**
  * Created by Admin on 25.06.2017.
  */
 public class Test {
     public static void main(String[] args) {
-        System.out.println("hello world");
 
-        System.out.println("все получилось");
+        Scanner sc = new Scanner(System.in);
 
-        System.out.println("WATAFUCK??");
+        int age;
+        //sc.nextLine();//java bug
+
+        while (true) {
+            try {
+                System.out.println("Введите возраст");
+                age = sc.nextInt();
+                sc.nextLine();//java bug
+                break;
+            } catch (Exception e) {
+                System.out.println("Упс, это не число!!! может все таки - ");
+                sc.nextLine();//java bug
+            }
+        }
+
+
+
     }
 }
