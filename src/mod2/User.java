209f2a9932167184d20687b2d914d446f7f97416
filpenkosan9 <filipenkosan9@mail.ru>
2017-07-14 21:52:21 +0300

    .

--- src/mod2/User.java
+++ src/mod2/User.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class User {
 
-    private static String name ;
+    private static String name;
     private static String city;
     private static String hobby;
     private static int age;
@@ -13,14 +13,13 @@ public class User {
 
     public static void main(String[] args) {
         System.out.println("Введите ваше имя");
-         name = in.nextLine();
+        name = in.nextLine();
 
         System.out.println("Введите ваш город");
-         city = in.nextLine();
+        city = in.nextLine();
 
         System.out.println("Ваше хобби?");
-         hobby = in.nextLine();
-
+        hobby = in.nextLine();
 
 
         while (true) {
@@ -37,31 +36,34 @@ public class User {
 
         vuborVarianta();
     }
-    public static void vuborVarianta (){
+
+    public static void vuborVarianta() {
         System.out.println("Выберите вариант");
         int s = in.nextInt();
-        while (s != 4 ){
+        while (s != 4) {
 
-            if (s >=1 && s < 4){
+            if (s >= 1 && s < 4) {
 
-            if (s == 1){
-                System.out.println("Имя: " + name + "\nГород: " + city + "\nВозоаст: " +
-                        "" + age + "\nХобби: " + hobby + "\n");
-                break;}
+                if (s == 1) {
+                    System.out.println("Имя: " + name + "\nГород: " + city + "\nВозоаст: " +
+                            "" + age + "\nХобби: " + hobby + "\n");
+                    break;
+                }
                 if (s == 2) {
                     System.out.println("Человек по имени " + name + " живет в городе " + city + ".\n" +
                             "Этому человеку " + age + " лет и любит он заниматься " + hobby + ".\n");
                     break;
                 }
-                if (s == 3){
+                if (s == 3) {
                     System.out.println(name + " - имя\n" + city + " - город" +
                             "\n" + age + " - возраст\n" + hobby + " - хобби");
                     break;
 
 
-            }}in.nextInt();
+                }
+            }
+            in.nextInt();
         }
-
 
 
     }
