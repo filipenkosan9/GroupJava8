686af26a4e37d576330e1dc2eda45f98d6446b41
filpenkosan9 <filipenkosan9@mail.ru>
2017-10-05 22:47:40 +0300

    v1

--- src/carsMod5/CarWheel.java
+++ src/carsMod5/CarWheel.java
@@ -4,4 +4,16 @@ package carsMod5;
  * Created by Admin on 04.10.2017.
  */
 public class CarWheel {
+    private float StatusWheel;
+
+    public CarWheel() {
+        StatusWheel = 1.0f;
+    }
+
+    public CarWheel(float statusWheel) {
+
+        StatusWheel = statusWheel;
+    }
+
+    public 
 }
